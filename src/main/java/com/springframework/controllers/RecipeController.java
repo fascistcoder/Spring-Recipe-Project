efc2a53b17903f490cfe657b467c43ff1515df38
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-27 12:03:54 +0530

    Display a Recipe by ID

---
+++ src/main/java/com/springframework/controllers/RecipeController.java
@@ -0,0 +1,27 @@
+package com.springframework.controllers;
+
+import com.springframework.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 27/09/21
+ */
+@Controller
+public class RecipeController {
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        return "recipe/show";
+    }
+}
---
+++ src/test/java/com/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,50 @@
+package com.springframework.controllers;
+
+import com.springframework.model.Recipe;
+import com.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 27/09/21
+ */
+class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController recipeController;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        MockitoAnnotations.openMocks(this);
+
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    void testGetRecipe() throws Exception{
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}

20c518b66725c3ddb9c2c9c64be4ada5081899bf
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-10-02 13:14:25 +0530

    Spring MVC Exception Handler

--- src/main/java/com/springframework/controllers/RecipeController.java
+++ src/main/java/com/springframework/controllers/RecipeController.java
@@ -1,12 +1,15 @@
 package com.springframework.controllers;
 
 import com.springframework.commands.RecipeCommand;
+import com.springframework.exceptions.NotFoundException;
 import com.springframework.services.RecipeService;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
@@ -51,5 +54,14 @@ public class RecipeController {
         log.debug("Deleting id: " + id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 }
--- src/test/java/com/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/controllers/RecipeControllerTest.java
@@ -57,14 +57,14 @@ class RecipeControllerTest {
 
     @Test
     void testGetRecipeNotFound() throws Exception{
-//        Exception exception = Assertions.assertThrows()
         Recipe recipe = new Recipe();
         recipe.setId(1L);
 
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 
     @Test
